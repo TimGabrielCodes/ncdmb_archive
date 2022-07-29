5f88f4a3d26e926aa3d742ab4c06a1544ae65c42
TimGabriel1 <zinwotatimothy@gmail.com>
2022-07-29 09:10:57 +0100

    Init

---
+++ src/main/java/ncdmb/ncdmb_archive/NcdmbArchiveApplication.java
@@ -0,0 +1,13 @@
+package ncdmb.ncdmb_archive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcdmbArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcdmbArchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ncdmb/ncdmb_archive/NcdmbArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ncdmb.ncdmb_archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcdmbArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
